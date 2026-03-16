24d06401050952881a4c0192f3eadeb3f106d455
giftyindia <mangogaming811@gmail.com>
2026-03-17 00:12:32 +0530

    Upgrade cache name to version 2 and improve fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = "money-mage-v1";
+const CACHE_NAME = "money-mage-v2";
 
 const urlsToCache = [
+  "/",
   "index.html",
   "service.html",
   "support.html",
@@ -10,16 +11,38 @@ const urlsToCache = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+
+    caches.match(event.request)
+      .then(response => {
+
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request)
+          .then(networkResponse => {
+
+            return caches.open(CACHE_NAME)
+              .then(cache => {
+
+                cache.put(event.request, networkResponse.clone());
+
+                return networkResponse;
+
+              });
+
+          });
+
+      })
+
   );
+
 });
