47f959292cc691e5cf99b7944f9683f9c2f7549d
giftyindia <mangogaming811@gmail.com>
2026-03-25 17:46:51 +0530

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,24 @@
+document.querySelector("form").addEventListener("submit", async function(e) {
+    e.preventDefault();
 
+    const name = document.getElementById("input-name").value;
+    const email = document.getElementById("input-email").value;
+    const topic = document.getElementById("input-topic").value;
+    const message = document.getElementById("input-message").value;
+
+    try {
+        const res = await fetch("http://localhost:5000/feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ name, email, topic, message })
+        });
+
+        const data = await res.json();
+        alert(data.message);
+    } catch (error) {
+        alert("Error sending feedback ❌");
+        console.log(error);
+    }
+});
